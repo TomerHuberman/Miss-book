20e0a8ef7f3118d2cdcbe58f40b08b5ab9562037
shrfrn <shrfrn@gmail.com>
2023-02-26 11:51:34 +0200

    Delete a car

--- cmps/CarIndex.js
+++ cmps/CarIndex.js
@@ -3,12 +3,23 @@ import { carService } from '../services/car.service.js'
 export default {
     template: `
         <section class="car-index">
-            <CarList :cars="cars"/>
+            <CarList :cars="cars" @remove="removeCar"/>
         </section>
     `,
     data() {
         return {
             cars: null,
+        }
+    },
+    methods: {
+        removeCar(carId) {
+            carService.remove(carId)
+                .then(() => {
+                    const idx = this.cars.findIndex(car => car.id === carId)
+                    this.cars.splice(idx, 1)
+                    // carService.query()
+                    //     .then(cars => this.cars = cars)
+                })
         }
     },
     created() {
--- cmps/CarList.js
+++ cmps/CarList.js
@@ -5,8 +5,14 @@ export default {
             <ul>
                 <li v-for="car in cars">
                     <CarPreview :car="car"/>
+                    <button @click="remove(car.id)">x</button>
                 </li>
             </ul>
         </section>
     `,
+    methods: {
+        remove(carId) {
+            this.$emit('remove', carId)
+        }
+    }
 }
