f520ec9c9f29e87cc82269402ff51241a76cef45
Tomer Huberman <tomerh1999@gmail.com>
2023-02-27 17:13:38 +0200

    all good no reviews

--- cmps/BookDetails.js
+++ cmps/BookDetails.js
@@ -2,11 +2,13 @@ export default {
     props: ['book'],
     template: `
         <section class="book-details">
+            <img v-if="book.listPrice.isOnSale" class="sale" src="../assets/img/sale.png" />
             <h2>{{ book.title }}</h2>
-            <h3 :class="lowOrHighClass">{{ book.listPrice.amount }}</h3>
+            <img class="book-img" :src="book.thumbnail" />
+            <h3 >Price: <span :class="lowOrHighClass">{{ book.listPrice.amount }}</span> {{ book.listPrice.currencyCode }} </h3>
             <h3>{{ readingLevel }}</h3>
             <h3>{{ newOrOld }}</h3>
-            <h3>{{ onSale }}</h3>
+            <!-- <h3>{{ onSale }}</h3> -->
 
             <button @click="closeDetails">Close</button>
         </section>
@@ -15,11 +17,11 @@ export default {
         closeDetails() {
             this.$emit('hide-details')
         },
-  
+
     },
     computed: {
         readingLevel() {
-            if (this.book.pageCount < 100) return 'Light Reading'
+            if (this.book.pageCount < 200) return 'Light Reading'
             if (this.book.pageCount > 500) return 'Serious Reading'
             if (this.book.pageCount > 200) return 'Descent Reading'
         },
@@ -35,8 +37,8 @@ export default {
                 'low-price': this.book.listPrice.amount < 20,
             }
         },
-        onSale(){
-            return this.book.listPrice.isOnSale ? 'ON SALE!' : ''
-        }
+        // onSale(){
+        //     return this.book.listPrice.isOnSale ? 'ON SALE!' : ''
+        // }
     }
 }
--- cmps/BookPreview.js
+++ cmps/BookPreview.js
@@ -2,9 +2,10 @@ export default {
     props: ['book'],
     template: `
         <article class="book-preview">
+            <img v-if="book.listPrice.isOnSale" class="sale" src="../assets/img/sale.png" />
             <h2>{{ book.title }}</h2>
             <img :src="book.thumbnail" />
-            <h3 :class="lowOrHighClass">{{ book.listPrice.amount }}</h3>
+            <h3 >Price: <span :class="lowOrHighClass">{{ book.listPrice.amount }}</span> {{ book.listPrice.currencyCode }} </h3>
         </article>
     `,
     computed: {
